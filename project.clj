de16ca9afd3095903f587f01f1adaf399968bf38
Paula Gearon <pgearon@cisco.com>
2017-08-10 16:21:38 -0400

    Changed dependency to Clojure 1.8.0 and bumped version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject quoll/clients "0.1.0-SNAPSHOT"
+(defproject quoll/clients "0.1.1-SNAPSHOT"
   :description "Functions for clients to talk to services."
   :url "https://github.com/quoll/clients"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.4.0"]])
 
