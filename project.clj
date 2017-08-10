5d0374812dbf23b0d1eeefeedbfecfb873e51a28
Paula Gearon <pgearon@cisco.com>
2017-08-10 15:20:42 -0400

    Updated doc on project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject quoll/clients "0.1.0-SNAPSHOT"
-  :description "Functions for clients to services."
-  ; :url "http://example.com/FIXME"
+  :description "Functions for clients to talk to services."
+  :url "https://github.com/quoll/clients"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
